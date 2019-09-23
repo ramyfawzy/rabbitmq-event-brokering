c146dd042930c4f22bed7cb7c58212f2134cf411
ribrahim <r.ibrahim@bigpoint.net>
2019-09-23 12:47:13 +0200

    Initial

---
+++ src/main/java/com/bp/broker/RabbitmqEventBrokeringApplication.java
@@ -0,0 +1,13 @@
+package com.bp.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqEventBrokeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqEventBrokeringApplication.class, args);
+	}
+
+}
